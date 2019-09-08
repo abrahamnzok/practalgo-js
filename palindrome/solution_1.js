411cb3feebd17646af58ceb1fd9237cbbc213fc8
amubanzo <ab.learn.code@gmail.com>
2019-09-09 01:08:34 +0200

    [interviews][palindrome-next] . Changed word parameter name to sequence . Added a new file and unit test case for the ultimate palindrome algorithm implementations.

--- palindrome/solution_1.js
+++ palindrome/solution_1.js
@@ -1,23 +1,23 @@
 const solution_1 = {};
 
 /**
- *
- * @param word The word to reverse
+ * Reveres a string
+ * @param sequence The word to reverse
  * @returns {string} A new string but reversed
  */
-const reversedWord = word => word === "" ? "" : reversedWord(word.substr(1)) + word.charAt(0);
+const reversedWord = sequence => sequence === "" ? "" : reversedWord(sequence.substr(1)) + sequence.charAt(0);
 
 /**
  *
- * @param word A String
+ * @param sequence A String
  * @param reversedWord Function to reverse the @word
  * @returns {boolean} True if the first parameter is equal to its reversed instance
  */
-solution_1.isPalindrome = (word, reversedWord) => word === reversedWord(word);
+solution_1.isPalindrome = (sequence, reversedWord) => sequence === reversedWord(sequence);
 
 /**
  *
- * @param word The word or sequence to verify
+ * @param sequence The word or sequence to verify
  * @returns {boolean}
  */
-module.exports = word => solution_1.isPalindrome(word, reversedWord);
+module.exports = sequence => solution_1.isPalindrome(sequence, reversedWord);
