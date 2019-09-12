2967ccabb6cf9fc646b0db3244f397ce1cf00441
amubanzo <ab.learn.code@gmail.com>
2019-09-12 17:05:18 +0200

    [interviews][fibonacci-next] ° updated fibonacci's readme ° Added a new fibonacci.js file ° refactored solution_1.js ° updated package.json name field.

--- fibonacci/solution_1.js
+++ fibonacci/solution_1.js
@@ -5,21 +5,22 @@ const solution_1 = {};
  * @param n A integer from 0 onwards
  * @returns {number}
  */
-solution_1.firstChild = n => (n <= 0 ? 0 : n === 1 ? 0 : n);
+const firstChild = n => (n <= 0 ? 0 : n === 1 ? 0 : n);
 
 /**
  * Given n, finds the next number in the fibonacci sequence
  * @param n An integer from 0 onward
+ * @param firstChild
  * @returns {number|Node}
  */
-solution_1.fibonacci = n =>
-  this.firstChild(n) === 0 || this.firstChild(n) === 1
-    ? this.firstChild(n)
-    : this.fibonacci(n - 2) + this.fibonacci(n - 1);
+solution_1.fibonacci = (n, firstChild) =>
+    firstChild(n) === 0 || firstChild(n) === 1
+        ? firstChild(n)
+        : this.fibonacci(n - 2) + this.fibonacci(n - 1);
 
 /**
  *
  * @param n
  * @returns {*}
  */
-module.exports = n => solution_1.fibonacci(n);
+module.exports = n => solution_1.fibonacci(n,firstChild);
