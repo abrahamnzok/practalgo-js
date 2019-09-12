1481d6a7f3f791aad3c487d36d76eea73a08df36
amubanzo <ab.learn.code@gmail.com>
2019-09-12 17:47:24 +0200

    [interviews][fibonacci-next] ° refactored solution 1 & 2 ° added a test file

--- fibonacci/solution_1.js
+++ fibonacci/solution_1.js
@@ -1,5 +1,3 @@
-const solution_1 = {};
-
 /**
  * Returns 0 if n is lower or equal to 0 or n is equal to 1 and n otherwise
  * @param n A integer from 0 onwards
@@ -11,16 +9,17 @@ const firstChild = n => (n <= 0 ? 0 : n === 1 ? 0 : n);
  * Given n, returns the next number in the fibonacci sequence
  * @param n An number from 0 onwards
  * @param firstChild
+ * @param {function} fibonacci
  * @returns {number|Node} a number
  */
-solution_1.fibonacci = (n, firstChild) =>
+const fibonacci = (n, firstChild, fibonacci) =>
     firstChild(n) === 0 || firstChild(n) === 1
         ? n
-        : this.fibonacci(n - 2) + this.fibonacci(n - 1);
+        : fibonacci(n - 2, firstChild, fibonacci) + fibonacci(n - 1, firstChild, fibonacci);
 
 /**
  * Invoke this function to find the fibonacci sequence
  * @param n a number
  * @returns {*} the next number in the fibonacci sequence
  */
-module.exports = n => solution_1.fibonacci(n,firstChild);
+module.exports = n => fibonacci(n,firstChild, fibonacci);
--- fibonacci/solution_2.js
+++ fibonacci/solution_2.js
@@ -1,17 +1,16 @@
-const solution_2 = {};
-
 /**
  * Given n, finds the next number in the fibonacci sequence
  * @param n An integer from 0 onwards
+ * @param {function} fibonacci
  * @returns {number|Node} the next number in the fibonacci sequence
  */
-solution_2.fibonacci = n => {
+const fibonacci = (n, fibonacci) => {
   if (n === 0) {
     return 0;
   } else if (n === 1) {
     return 1;
   } else {
-    return this.fibonacci(n - 2) + this.fibonacci(n - 1);
+    return fibonacci(n - 2, fibonacci) + fibonacci(n - 1, fibonacci);
   }
 };
 
@@ -20,4 +19,4 @@ solution_2.fibonacci = n => {
  * @param n a number
  * @returns {*} the next number in the fibonacci sequence
  */
-module.exports = n => solution_2.fibonacci(n);
+module.exports = n => fibonacci(n, fibonacci);
